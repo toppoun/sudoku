eff444eb3de22d22148b3ef4f5d1ac0987008fc9
toppoun <kokisuke0000@gmail.com>
2026-01-11 17:58:36 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+TEXT_PATH = Path("sudoku-data")
+PICT_PATH_TIMES = Path("sudoku-picture-times")
+PICT_PATH_HELVETICA = Path("sudoku-picture-helvetica")
+PICT_PATH_COURIER = Path("sudoku-picture-courier")
+SKEW_PATH = Path("sudoku-skew")
+
+DEBUG = True
